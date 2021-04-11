58069a574ea7d46c313fecf9fda99fe5974e9e6a
MaideAkdede <maide.akdede@student.hepl.com>
2021-04-11 15:10:39 +0200

    :pencil2: modify path name

--- configs/config.php
+++ configs/config.php
@@ -2,4 +2,4 @@
 setlocale(LC_TIME, "fr_BE");
 define('TODAY', strftime('%e %B %G'));
 define('FORMAT_DATE', strftime('%Y-%m-%d'));
-define('FILE_PATH', 'data/matches.csv');
+define('FILE_PATH', 'datas/matches.csv');
