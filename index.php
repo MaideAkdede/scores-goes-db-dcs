43eeddae696c1d8f86097a6bd5e6730455a1a7c8
MaideAkdede <maide.akdede@student.hepl.com>
2021-05-02 17:43:29 +0200

    :pencil2: vidéo 9 commit

--- index.php
+++ index.php
@@ -1,79 +1,93 @@
 <?php
-
 use function Team\all as teamAll;
 use function Match\allWithTeams as allMatchesWithTeams;
 use function Match\allMatchesWithTeamsGrouped as allMatchesWithTeamsGrouped;
 
 require('configs/config.php');
 require('utils/dbaccess.php');
+require('utils/standings.php');
 require('models/team.php');
 require('models/match.php');
 
 $pdo = getConnection();
 
-$standings = [];
-$matches2 =  allMatchesWithTeamsGrouped(allMatchesWithTeams($pdo));
-$teams = teamAll($pdo);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Traiter les trucs en POST
+    if (isset($_POST['action']) && isset($_POST['resource'])) {
+        if ($_POST['action'] === 'store' && $_POST['resource'] === 'match') {
 
-function getEmptyStatsArray()
-{
-    return [
-        'games' => 0,
-        'points' => 0,
-        'wins' => 0,
-        'losses' => 0,
-        'draws' => 0,
-        'GF' => 0,
-        'GA' => 0,
-        'GD' => 0,
-    ];
-}
+            $matchDate = $_POST['match-date'] ?: FORMAT_DATE;
+            $homeTeam = $_POST['home-team-unlisted'] === '' ? $_POST['home-team'] : $_POST['home-team-unlisted'];
+            $awayTeam = $_POST['away-team-unlisted'] === '' ? $_POST['away-team'] : $_POST['away-team-unlisted'];
+            $homeTeamGoals = $_POST['home-team-goals'] ?: 0;
+            $awayTeamGoals = $_POST['away-team-goals'] ?: 0;
 
-foreach ($matches2 as $match){
-    $homeTeam = $match->home_team;
-    $awayTeam = $match->away_team;
+            $match = [$matchDate, $homeTeam, $homeTeamGoals, $awayTeamGoals, $awayTeam];
 
-    if (!array_key_exists($homeTeam, $standings)) {
-        $standings[$homeTeam] = getEmptyStatsArray();
-    }
-    if (!array_key_exists($awayTeam, $standings)) {
-        $standings[$awayTeam] = getEmptyStatsArray();
-    }
-    $standings[$homeTeam]['games']++;
-    $standings[$awayTeam]['games']++;
-
-    if ($match->home_team_goals === $match->away_team_goals) {
-        $standings[$homeTeam]['points']++;
-        $standings[$awayTeam]['points']++;
-        $standings[$homeTeam]['draws']++;
-        $standings[$awayTeam]['draws']++;
-    } elseif ($match->home_team_goals > $match->away_team_goals) {
-        $standings[$homeTeam]['points'] += 3;
-        $standings[$homeTeam]['wins']++;
-        $standings[$awayTeam]['losses']++;
-    } else {
-        $standings[$awayTeam]['points'] += 3;
-        $standings[$awayTeam]['wins']++;
-        $standings[$homeTeam]['losses']++;
-    }
-    $standings[$homeTeam]['GF'] += $match->home_team_goals;
-    $standings[$homeTeam]['GA'] += $match->away_team_goals;
-    $standings[$awayTeam]['GF'] += $match->away_team_goals;
-    $standings[$awayTeam]['GA'] += $match->home_team_goals;
-    $standings[$homeTeam]['GD'] += $standings[$homeTeam]['GF'] - $standings[$homeTeam]['GA'];
-    $standings[$awayTeam]['GD'] += $standings[$awayTeam]['GF'] - $standings[$awayTeam]['GA'];
-
-}
-
-uasort($standings, function ($homeTeam, $awayTeam) {
-    if ($homeTeam['points'] === $awayTeam['points']) {
-        if ($homeTeam['GD'] > $awayTeam['GD']) {
-            return -1;
-        } else {
-            return 1;
+            //TODO : ajouter données entrées dans la DB
         }
-    }
-    return $homeTeam['points'] > $awayTeam['points'] ? -1 : 1;
-});
 
+    }
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Traiter les trucs en GET
+    if (!isset($_GET['action']) && !isset($_GET['resource'])) {
+        // HOMEPAGE
+        $standings = [];
+        $matches2 = allMatchesWithTeamsGrouped(allMatchesWithTeams($pdo));
+        $teams = teamAll($pdo);
+
+        foreach ($matches2 as $match) {
+            $homeTeam = $match->home_team;
+            $awayTeam = $match->away_team;
+
+            if (!array_key_exists($homeTeam, $standings)) {
+                $standings[$homeTeam] = getEmptyStatsArray();
+            }
+            if (!array_key_exists($awayTeam, $standings)) {
+                $standings[$awayTeam] = getEmptyStatsArray();
+            }
+            $standings[$homeTeam]['games']++;
+            $standings[$awayTeam]['games']++;
+
+            if ($match->home_team_goals === $match->away_team_goals) {
+                $standings[$homeTeam]['points']++;
+                $standings[$awayTeam]['points']++;
+                $standings[$homeTeam]['draws']++;
+                $standings[$awayTeam]['draws']++;
+            } elseif ($match->home_team_goals > $match->away_team_goals) {
+                $standings[$homeTeam]['points'] += 3;
+                $standings[$homeTeam]['wins']++;
+                $standings[$awayTeam]['losses']++;
+            } else {
+                $standings[$awayTeam]['points'] += 3;
+                $standings[$awayTeam]['wins']++;
+                $standings[$homeTeam]['losses']++;
+            }
+            $standings[$homeTeam]['GF'] += $match->home_team_goals;
+            $standings[$homeTeam]['GA'] += $match->away_team_goals;
+            $standings[$awayTeam]['GF'] += $match->away_team_goals;
+            $standings[$awayTeam]['GA'] += $match->home_team_goals;
+            $standings[$homeTeam]['GD'] += $standings[$homeTeam]['GF'] - $standings[$homeTeam]['GA'];
+            $standings[$awayTeam]['GD'] += $standings[$awayTeam]['GF'] - $standings[$awayTeam]['GA'];
+
+        }
+
+        uasort($standings, function ($homeTeam, $awayTeam) {
+            if ($homeTeam['points'] === $awayTeam['points']) {
+                if ($homeTeam['GD'] > $awayTeam['GD']) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+            return $homeTeam['points'] > $awayTeam['points'] ? -1 : 1;
+        });
+
+
+    }
+} else {
+    // Si ni POST ni GET alors rediriger à Index et Stoppé l'exécution du script
+    header('Location: ../index.php');
+    exit();
+}
 require('views/vue.php');
